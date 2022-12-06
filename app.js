311d85b75724e16c5a8514fb1b51a2206d6c8c75
Nate Malone <118545949+Nate-Malone@users.noreply.github.com>
2022-12-06 11:59:32 -0500

    Update app.js

--- app.js
+++ app.js
@@ -1,14 +1,20 @@
-'use strict';
-
-const express = require('express')
+"use strict";
+require("dotenv").config();
+const express = require("express");
 const app = express();
 
 const port = 8080;
-const host = '0.0.0.0';
+const host = "0.0.0.0";
 
-app.get('/', (req, res) => {
-  res.send('Hello World from IBM Cloud Essentials!');
-})
+app.get("/", (req, res) => {
+  res.send(
+    `Hello World from IBM Cloud Essentials!<br/><br/>
+    Environment Variables:
+    <br/>MY_VAR_1=${process.env.MY_VAR_1}<br/>
+    <br/>MY_VAR_2=${process.env.MY_VAR_2}<br/>
+    <br/>MY_VAR_3=${process.env.MY_VAR_3}<br/>`
+  );
+});
 
 app.listen(port, host);
 console.log(`Running on http://${host}:${port}`);
